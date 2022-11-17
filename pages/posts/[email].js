31128b7aaf1cd2549cf855a5229738adbc00af01
hyunjin1012 <hyunjin1012@gmail.com>
2022-11-17 11:37:22 -0800

    edit posts page

--- pages/posts/[email].js
+++ pages/posts/[email].js
@@ -32,35 +32,35 @@ export default function Posts({ posts, email }) {
   const router = useRouter();
   if (!router.isReady) {
     return <div className="h-screen">Loading...</div>;
-  }
-  return (
-    <div>
-      <Seo title={email + "'s world"} />
-      <main>
-        <div className="flex flex-col items-center justify-center gap-3 p-8 min-h-screen">
-          {posts?.map((post, index) => (
-            <div
-              key={index}
-              className="w-[90%] rounded overflow-hidden shadow-lg p-5 md:w-[80%] lg:w-[65%]"
-            >
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{post.title}</div>
-                <div className="text-gray-700 text-base">{post.content}</div>
-              </div>
-              <div className="px-6 py-4 flex justify-end">
-                {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+  } else
+    return (
+      <div>
+        <Seo title={email + "'s world"} />
+        <main>
+          <div className="flex flex-col items-center justify-center gap-3 p-8 min-h-screen">
+            {posts?.map((post, index) => (
+              <div
+                key={index}
+                className="w-[90%] rounded overflow-hidden shadow-lg p-5 md:w-[80%] lg:w-[65%]"
+              >
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{post.title}</div>
+                  <div className="text-gray-700 text-base">{post.content}</div>
+                </div>
+                <div className="px-6 py-4 flex justify-end">
+                  {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               written {timeAgo(post.createdAt)}
             </span> */}
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700 mr-2 mb-2">
-                  updated {timeAgo(post.updatedAt)}
-                </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700 mr-2 mb-2">
+                    updated {timeAgo(post.updatedAt)}
+                  </span>
+                </div>
+                <PostComment postId={post.id} />
+                <ViewComments postId={post.id} />
               </div>
-              <PostComment postId={post.id} />
-              <ViewComments postId={post.id} />
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
-  );
+            ))}
+          </div>
+        </main>
+      </div>
+    );
 }
